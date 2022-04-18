75183eb0cbb2ba028018569907fb4b7f24e88716
Yugesh Palvai <yugeshpalvai21@gmail.com>
2022-04-18 19:29:56 +0530

    Adds default props to Tasks component and implement conditional rendering in Tasks component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -3,9 +3,13 @@ import Task from './Task.js';
 const Tasks = (props) => {
   return (
     <div>
-      { props.tasks.map((task) => <Task task={task} />) }
+      { props.tasks.length > 0 ? props.tasks.map((task) => <Task task={task} />) : "No Tasks" }
     </div>
   )
+}
+
+Tasks.defaultProps = {
+  tasks: []
 }
 
 export default Tasks;
