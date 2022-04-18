b147c28bb805d7606d0c753dfb62d4d8da858540
Yugesh Palvai <yugeshpalvai21@gmail.com>
2022-04-18 18:05:32 +0530

    instantials Tasks state on App component and render all tasks using JSX expression

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 const App = () => {
+  const tasks = [{
+    title: 'First Task',
+    description: 'small description about first task',
+    reminder: false,
+    finished: false
+  },
+  {
+    title: 'Second Task',
+    description: 'small description about second task',
+    reminder: false,
+    finished: false
+  }]
+
   return (
     <div>
       <h1>Welcome To Task Trackeeeer App</h1>
+      {tasks.map((task) => (<p>{task.title}</p>))}
     </div>
   )
 }
